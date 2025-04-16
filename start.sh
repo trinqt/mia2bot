1284d76413cf9761023b518feb61f243eaee2a52
NQT <156076323+trinqt@users.noreply.github.com>
2025-04-17 01:20:58 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,9 @@
-
 #!/data/data/com.termux/files/usr/bin/bash
 
 # Thông số Telegram Bot
 BOT_TOKEN="7661043177:AAEL1xO9C1O4vMnr705gZvPPRMh5JN26VHk"
 CHAT_ID="5197540151"
-
-# Cổng SSH mặc định Termux
 SSH_PORT=8022
-
-# Tên user hiện tại
 USERNAME=$(whoami)
 
 # Khởi động SSH nếu chưa chạy
@@ -17,10 +12,15 @@ if ! pgrep -x sshd >/dev/null; then
   sleep 2
 fi
 
-# Lấy IP (thay wlan0 nếu bạn dùng khác)
-IP_ADDRESS=$(ip addr show wlan0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
+# Lấy IP (fallback qua hostname -I)
+IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
-# Tạo lệnh SSH
+# Nếu vẫn trống, thử ip route
+if [ -z "$IP_ADDRESS" ]; then
+  IP_ADDRESS=$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++) if($i=="src") print $(i+1)}')
+fi
+
+# Chuẩn bị câu lệnh SSH
 SSH_CMD="ssh -p $SSH_PORT $USERNAME@$IP_ADDRESS"
 
 # Gửi nguyên lệnh SSH qua Telegram
@@ -28,5 +28,4 @@ curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
   -d chat_id="$CHAT_ID" \
   -d text="$SSH_CMD"
 
-# Thông báo đã gửi
-echo "SSH command sent: $SSH_CMD"
+echo "Đã gửi: $SSH_CMD"
