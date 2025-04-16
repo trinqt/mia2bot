5e997527433cf9bc1543bfa9b0370678b3c0b7f3
NQT <156076323+trinqt@users.noreply.github.com>
2025-04-16 21:33:06 +0700

    start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,12 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
 
-# Bật SSH server
-echo "Bật SSH server..."
-sshd
-
-# Lấy địa chỉ IP của máy và gửi vào Telegram bot
-IP=$(hostname -I | awk '{print $1}')
-echo "IP hiện tại: $IP"
-
-# Gửi IP vào Telegram bot (bạn cần thay thế TOKEN và CHAT_ID)
-TOKEN="7661043177:AAEL1xO9C1O4vMnr705gZvPPRMh5JN26VHk"
-CHAT_ID="5197540151"
-curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d chat_id=$CHAT_ID -d text="IP hiện tại của bạn là: $IP"
-
-# Chạy bot (nếu có)
+# Khởi động Flask Server
 cd $HOME/botmia2
-python bot.py
+nohup python web.py > /dev/null 2>&1 &
 
-# Hoặc nếu bạn dùng các dịch vụ khác, ví dụ chạy bot Telegram từ GitHub
-# git pull
-# python bot.py
+# Khởi động Telegram Bot
+cd $HOME/botmia2
+nohup python bot.py > /dev/null 2>&1 &
+
+# Khởi động Cloudflare Tunnel
+nohup cloudflared tunnel run mia2bot > /dev/null 2>&1 &
