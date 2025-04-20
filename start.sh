aee33638e152da8c1874a067d8eb3f48b4ab4c62
NQT <156076323+trinqt@users.noreply.github.com>
2025-04-21 00:00:54 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # Chạy bot Python từ GitHub ở chế độ nền
 
-curl -s https://raw.githubusercontent.com/trinqt/mia2bot/refs/heads/main/ssh.py | nohup python3 - > /dev/null 2>&1 &
-
 curl -s https://raw.githubusercontent.com/trinqt/mia2bot/refs/heads/main/bot.py | nohup python3 - > /dev/null 2>&1 &
